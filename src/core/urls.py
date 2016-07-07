5447c97241a09f7f8d3105347143e9aed164c2f5
Kamil Kujawinski <kamil@kujawinski.net>
2016-07-07 06:22:37 +0200

    fixed deploying statics

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,16 +1,16 @@
 # coding: utf-8
 # Core and 3th party packages
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
     url(r'', include(admin.site.urls)),
     # add api urls and allow to be passed only them in certain domains
     # they can't overlap with admin
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# from django.conf import settings
-# from django.conf.urls.static import static
 # from django.views.i18n import javascript_catalog
 
 # # Project imports
@@ -23,4 +23,4 @@ urlpatterns = [
 #         namespace='rest_framework')),
 #     url(r'^jsi18n/$', javascript_catalog, name='javascript-catalog'),
 #     url(r'^api/urls/$', UrlsApi.as_view(), name='api_urls'),
-# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ]
